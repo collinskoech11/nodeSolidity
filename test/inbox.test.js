83e43879ff8d047cb8aebff0ed153280e12b80d3
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-25 12:25:22 +0300

    set uptesting modules

---
+++ test/inbox.test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const  ganache = require('ganache-cli');
+const Web3 = require('Web3');// cAPITAL Is a constructor function 
+
+//assign an instance to work with variable that is lowercase for web3
