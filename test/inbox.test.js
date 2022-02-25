018a6d8dcb593be64e642a26dfc54a9ac28e1ae5
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-25 13:47:47 +0300

    setup WEb3 Instance

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const  ganache = require('ganache-cli');
-const Web3 = require('Web3');// cAPITAL Is a constructor function 
+const Web3 = require('web3');// cAPITAL Is a constructor function 
 
-//assign an instance to work with variable that is lowercase for web3
+//assign an instance to work with variable that is lowercase for web3 
+const web3 = new Web3(ganache.provider());// creates an instance Of Web3 and tells it to try to connect to the local test network for testing 
