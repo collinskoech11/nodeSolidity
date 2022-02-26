77bccc49558774f8582e621710c9988c7750f047
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-27 01:27:31 +0300

    deploy contract using anache generated accounts

--- deploy.js
+++ deploy.js
@@ -8,3 +8,15 @@ const {interface, bytecode} = require('./compile');
     'https://rinkeby.infura.io/v3/89fa897aa9aa4e88860156d1e65b331a'
  );
 const web3 = new Web3(provider);// take provider pass into the web3 and get an instance of web3
+
+const deploy = async () => {
+    //get all accounts unlocked by provider
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account',accounts[0])
+
+    await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send({ gas: '1000000', from: accounts[0]})
+};
+deploy();
