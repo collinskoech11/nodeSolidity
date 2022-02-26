3cb3b30dade5a105a62f3eb8d01f86c958cb1206
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-26 15:32:02 +0300

    get ganache accounts

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -5,6 +5,24 @@ const Web3 = require('web3');// cAPITAL Is a constructor function
 const web3 = new Web3(ganache.provider());// creates an instance Of Web3 and tells it to try to connect to the local test network for testing 
 
 
+let accounts;
+
+beforeEach(async () => {
+    // Get a list of all accounts 
+    accounts = await web3.eth.getAccounts()// web has many modules, in this case we'll be using the eth module
+    //on the eth module we are getting the getAccounts function 
+    // Almost every function that we call with web3 is an asyncronous function and returns a promise 
+    // .then(fetchedAccounts => {
+    //     console.log(fetchedAccounts)//displays the accounts fetched from ganache 
+    // })
+    // use one of the accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    });
+});
 // class Car {
 //     park() {//method
 //         return 'stopped';
