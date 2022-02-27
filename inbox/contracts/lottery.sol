19a3f55370f2fb95886880d5c11a119ddacc87a3
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-27 22:49:05 +0300

    added lottery contract

---
+++ inbox/contracts/lottery.sol
@@ -0,0 +1,24 @@
+pragma solidity  ^0.8.11; // specifies the solidity versionm in use 
+//version identifier
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;//msg is a global variable  will be referenced  
+    }
+    function enter() public payable{
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    function random() private view returns (uint){//sha3 global varible for sha hashing algorithm 
+        return  uint(keccak256(block.difficulty, now, players));
+    }
+    function pickWinner() public {
+        require(msg.sender == manager);//only the manager can cal pick winner fx
+        uint index = random() % players.length;//generate player winner index using modulo
+        players[index].transfer(this.balance);// this is a refference to an instance of the current contract 
+        players = new address[](0);//clear the players address array 
+    }
+}
+}
