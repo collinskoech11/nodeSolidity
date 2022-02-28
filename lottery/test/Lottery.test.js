aa5ce1c1e4356533a12d45a42abf3091414362c6
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-28 12:51:23 +0300

    edit test

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -64,4 +64,30 @@ describe('Lottery Contract', () => {
             assert(err);
         };
     });
+    it('only manager can call pickwinner fx', async () => {
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            })
+            assert(false);
+        }catch(err){
+            assert(err)
+        };
+    });
+    it('sends money  to winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2','ether')
+        });
+        const initialBalance = await web3.eth.getBalance();
+
+        await lottery.methods.pickWinner().send({ from:accounts[0] });
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+
+        console.log(difference)
+        assert(difference > web3.utils.toWei('1.8','ether'));
+    });
 });
