9cfcc39a769b82c30501c2dcbba1292b6b71d1b6
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-28 14:36:31 +0300

    added cra-template

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -80,14 +80,11 @@ describe('Lottery Contract', () => {
             value: web3.utils.toWei('2','ether')
         });
         const initialBalance = await web3.eth.getBalance(accounts[0]);
-
         await lottery.methods.pickWinner().send({ from:accounts[0] });
-
         const finalBalance = await web3.eth.getBalance(accounts[0]);
-
         const difference = finalBalance - initialBalance;
-
-        console.log(difference)
         assert(difference > web3.utils.toWei('1.8','ether'));
     });
+    //assert players is empty 
+    //assert that lottery balance is 0
 });
